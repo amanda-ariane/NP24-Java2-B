2d8e6880991468890f50dfe1255f38ccce91b369
amanda-ariane <amandaariane004@gmail.com>
2024-03-05 22:09:04 -0300

    Conteudo da aula do dia 05-03-2024

---
+++ NP24_java2_B/src/br/com/senaiso/bauru/amanda/secao02/Text01.java
@@ -0,0 +1,20 @@
+package br.com.senaiso.bauru.amanda.secao02;
+/**
+ * Comentário para documentação
+ *@param args
+ */
+public class Text01 {
+    public static void main(String[] args) {
+    	
+        System.out.println("   /\\         /\\   ");
+        System.out.println("  /  \\_______/  \\  ");
+        System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        {
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+        }
+    }    
+}
